61e4566f5253504ff6eedcffcdec24f9ce5f6a3b
Emir Sarı <emirsari@gmail.com>
2019-09-23 15:56:44 +0300

    initial commit

---
+++ Personal BrowserTests/Personal_BrowserTests.swift
@@ -0,0 +1,34 @@
+//
+//  Personal_BrowserTests.swift
+//  Personal BrowserTests
+//
+//  Created by Emir SARI on 23.09.2019.
+//  Copyright © 2019 Emir SARI. All rights reserved.
+//
+
+import XCTest
+@testable import Personal_Browser
+
+class Personal_BrowserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
